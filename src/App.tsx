546dacbfbf5df636a685ceee6004baf420fd73d6
Bartosz Bednarski <bambocha1997@gmail.com>
2024-07-04 23:30:50 +0200

    Added styles for RWD.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import classes from "./main.module.scss";
 import CardsBox from "./components/cardsBox/CardsBox";
